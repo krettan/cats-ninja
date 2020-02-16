722296a43c60259f469749eccb5437bba352998f
Carl Retzner <krettan@gmail.com>
2020-02-16 17:47:26 +0100

    WIP

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
 import Predictions from './components/predictions';
+import LandingPage from './components/LandingPage';
 import logo from './logo.svg';
 import './App.css';
 
+require('dotenv').config()
+
+const giphy = require('giphy-api')({
+  https: true,
+  apiKey: process.env.GIPHY_API_KEY
+});
+
 class App extends Component {
   state = {
-    predictions: []
+    predictions: [],
+    cats: [],
+    error: null,
   }
   
   componentDidMount() {
-    fetch('https://cats-ninja-api.herokuapp.com/predictions')
-      .then(res => res.json())
-      .then((data) => {
-        this.setState({ predictions: data })
+    giphy.search({
+      q: 'ninja cats',
+      limit: 10,
+      offset: Math.floor(Math.random() * 100) + 1  
+    })
+      .then(res => {
+        if (res.hasOwnProperty('data')) {
+          return res.data
+        } else {
+          console.log(res.message);
+          this.setState({ error: res.message })
+          return []
+        }
+      })
+      .then((cats) => {
+        this.setState({ cats });
       })
       .catch(console.log)
   }
   
   render () {
     return (
-      <Predictions predictions={this.state.predictions} />
+      <LandingPage cats={this.state.cats} error={this.state.error} />
     );
   }
 }
